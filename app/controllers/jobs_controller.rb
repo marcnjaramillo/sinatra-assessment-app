f6f0b9f276de77913e6cd818074a9f83f1686142
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-11-28 13:14:31 -0800

    Finish writing methods for users and jobs controllers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,10 +18,59 @@ class JobsController < ApplicationController
 
   post '/jobs' do
     if params[:location] || params[:type] || params[:duration] == ""
+      redirect to '/jobs/new'
     else
       current_user.jobs.create(location: params["location"], type: params["type"], duration: params["duration"])
-      session[:id] = @user.id
       redirect to "/jobs/#{@jobs.id}"
+    end
+  end
+
+  get '/jobs/:id' do
+    if logged_in?
+      @job = Job.find_by_id(params[:id])
+      erb :'jobs/show_job'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/jobs/:id/edit' do
+    if logged_in?
+      @job = Job.find_by_id(params[:id])
+      if @job.user_id == current_user.id
+       erb :'jobs/edit_job'
+      else
+        redirect to '/jobs'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/jobs/:id' do
+    if params[:content] == ""
+      redirect to "/jobs/#{params[:id]}/edit"
+    else
+      @job = Job.find_by_id(params[:id])
+      @job.location = params[:location]
+      @job.type = params[:type]
+      @job.duration = params[:duration]
+      @job.save
+      redirect to "/jobs/#{@job.id}"
+    end
+  end
+
+  delete '/jobs/:id/delete' do
+    if logged_in?
+      @job = Job.find_by_id(params[:id])
+      if @job.user_id == current_user.id
+        @job.delete
+        redirect to '/jobs'
+      else
+        redirect to '/job'
+      end
+    else
+      redirect to '/login'
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,31 @@ class UsersController < ApplicationController
       redirect to '/jobs'
     end
   end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/jobs'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/jobs"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
