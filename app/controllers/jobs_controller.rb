7bdaaf9d393e09f78af03796e7373ed7a56c26d3
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-11-28 12:21:03 -0800

    Begin writing methods for jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,3 +1,27 @@
 class JobsController < ApplicationController
+  get '/jobs' do
+    if logged_in?
+      @jobs = Job.all
+      erb :'jobs/details'
+    else
+      redirect to '/login'
+    end
+  end
 
+  get '/jobs/new' do
+    if logged_in?
+      erb :'jobs/create_job'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/jobs' do
+    if params[:location] || params[:type] || params[:duration] == ""
+    else
+      current_user.jobs.create(location: params["location"], type: params["type"], duration: params["duration"])
+      session[:id] = @user.id
+      redirect to "/jobs/#{@jobs.id}"
+    end
+  end
 end
